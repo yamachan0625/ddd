b03ae5bca4bae9839b82b968311375e9dbf737a9
yamachan0625 <napoli.080625@gmail.com>
2023-02-19 11:57:19 +0900

    UserName修正

--- XXXDomain/models/user/UserName/UserName.ts
+++ XXXDomain/models/user/UserName/UserName.ts
@@ -2,13 +2,16 @@ import { ValueObject } from '../../shared/ValueObject';
 
 export class UserName extends ValueObject<string> {
   static create(userName: string): UserName {
-    if (userName.length > 10 || userName.length < 1) {
+    if (
+      userName.length > this.MAX_LENGTH ||
+      userName.length < this.MIN_LENGTH
+    ) {
       throw new Error(
         `${this.MIN_LENGTH}文字以上${this.MAX_LENGTH}で指定してください`
       );
     }
 
-    if (!/^[a-zA-Zぁ-んァ-ンヴｧ-ﾝﾞﾟ\u4E00-\u9FFF]+$/.test(userName)) {
+    if (!this.REGEX.test(userName)) {
       throw new Error('許可されていない文字が含まれています');
     }
 
